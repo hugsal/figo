1f3bfb823c8c2402e8af81f29d29cb30b9b5b33d
Hugs <hugs.salazar@gmail.com>
2021-08-26 13:04:45 -0500

    Add router observable

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import { PostScreen } from "../components/post/PostScreen";
 
@@ -9,19 +9,18 @@ import { PrivateRoute } from "./PrivateRoute";
 import { PublicRoute } from "./PublicRoute";
 import { login } from "../actions/auth";
 
-const init = () => {
-  return localStorage.getItem("Token");
-};
-
 export const AppRouter = () => {
   const auth = useSelector((state) => state.auth);
+
+  const token = useMemo(() => {
+    return localStorage.getItem("Token") || auth.token;
+  }, [auth.token]);
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const isToken = init();
-    if (isToken) setIsLoggedIn(true);
-  }, []);
+    if (token) setIsLoggedIn(true);
+  }, [token]);
 
   return (
     <Router>
